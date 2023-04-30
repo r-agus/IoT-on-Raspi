74b81fa7f52c8452a25c1997e0622a5f745c9584
HaoFengChenFu <126356683+HaoFengChenFu@users.noreply.github.com>
2023-04-30 15:38:58 +0200

    Update server.cpp
    Errors solved

--- server.cpp
+++ server.cpp
@@ -70,9 +70,7 @@ void* terminatorThread(void*)
 
 int main(int argc, char *argv[])
 {
-	t_rcv_data data;
-	t_staditical_data data_processed;
-	
+
 	if (argc==1)
 	{
 		cout<<"Please pass the port number on which you want the server to listen."<<endl;
@@ -146,7 +144,6 @@ int main(int argc, char *argv[])
 		if(data.flags & 0x03){
 			generate_color_sensor_msg(data);
 			print_accel_msg(data);
-
 			printf("\n");
 			printf("%s\n", color_sensor_msg);
 			//printf("\033[3F");
@@ -163,7 +160,7 @@ int main(int argc, char *argv[])
 			generate_color_sensor_msg(data);
 			printf("%s", color_sensor_msg);
 		}
-*/		
+*/
 		calc_stadistics(data);
 		print_stadistics(data_mean, data_max, data_min, data_deviation);
 		fflush(stdout);
@@ -209,14 +206,32 @@ void calc_stadistics(t_rcv_data data_raw[]){
 	data_max = calc_maximum(data_raw);
 	data_min = calc_minimum(data_raw);
 	calc_deviation(data_raw, data_mean);
-]
+}
 
 t_rcv_data calc_mean(t_rcv_data data_raw[]){
 	t_rcv_data sum;
 	for(int i = 0; i < 10; i++){
-		sum += data_raw[i];
+		sum.acceleration.acc_x += data_raw[i].acceleration.acc_x;
+		sum.acceleration.acc_y += data_raw[i].acceleration.acc_y;
+		sum.acceleration.acc_z += data_raw[i].acceleration.acc_z;
+
+		sum.color.ir 	+= data_raw[i].color.ir;
+		sum.color.clear += data_raw[i].color.clear;
+		sum.color.red 	+= data_raw[i].color.red;
+		sum.color.green += data_raw[i].color.green;
+		sum.color.blue 	+= data_raw[i].color.blue;
+
 	}
-	return sum/10;
+	sum.acceleration.acc_x = sum.acceleration.acc_x/10;
+	sum.acceleration.acc_y = sum.acceleration.acc_y/10;
+	sum.acceleration.acc_z = sum.acceleration.acc_z/10;
+
+	sum.color.ir 	= sum.color.ir/10;
+	sum.color.clear = sum.color.clear/10;
+	sum.color.red 	= sum.color.red/10;
+	sum.color.green = sum.color.green/10;
+	sum.color.blue 	= sum.color.blue/10;
+	return sum;
 }
 
 t_rcv_data calc_maximum(t_rcv_data data_raw[]){
@@ -225,7 +240,7 @@ t_rcv_data calc_maximum(t_rcv_data data_raw[]){
 		max.acceleration.acc_x = (max.acceleration.acc_x > data_raw[i].acceleration.acc_x) ? max.acceleration.acc_x : data_raw[i].acceleration.acc_x;
 		max.acceleration.acc_y = (max.acceleration.acc_y > data_raw[i].acceleration.acc_y) ? max.acceleration.acc_y : data_raw[i].acceleration.acc_y;
 		max.acceleration.acc_z = (max.acceleration.acc_z > data_raw[i].acceleration.acc_z) ? max.acceleration.acc_z : data_raw[i].acceleration.acc_z;
-		
+
 		max.color.ir 	= (max.color.ir 	> data_raw[i].color.ir		) 	? max.color.ir 		: data_raw[i].color.ir;
 		max.color.clear = (max.color.clear 	> data_raw[i].color.clear	) 	? max.color.clear 	: data_raw[i].color.clear;
 		max.color.red 	= (max.color.red 	> data_raw[i].color.red		) 	? max.color.red 	: data_raw[i].color.red;
@@ -240,7 +255,7 @@ t_rcv_data calc_minimum(t_rcv_data data_raw[]){
 		min.acceleration.acc_x = (min.acceleration.acc_x > data_raw[i].acceleration.acc_x) ? min.acceleration.acc_x : data_raw[i].acceleration.acc_x;
 		min.acceleration.acc_y = (min.acceleration.acc_y > data_raw[i].acceleration.acc_y) ? min.acceleration.acc_y : data_raw[i].acceleration.acc_y;
 		min.acceleration.acc_z = (min.acceleration.acc_z > data_raw[i].acceleration.acc_z) ? min.acceleration.acc_z : data_raw[i].acceleration.acc_z;
-		
+
 		min.color.ir 	= (min.color.ir 	> data_raw[i].color.ir		) 	? min.color.ir 		: data_raw[i].color.ir;
 		min.color.clear = (min.color.clear 	> data_raw[i].color.clear	) 	? min.color.clear 	: data_raw[i].color.clear;
 		min.color.red 	= (min.color.red 	> data_raw[i].color.red		) 	? min.color.red 	: data_raw[i].color.red;
@@ -250,72 +265,72 @@ t_rcv_data calc_minimum(t_rcv_data data_raw[]){
 	return min;
 }
 void calc_deviation(t_rcv_data data_raw[], t_rcv_data mean){
-	
-	for(i = 0; i < 10; i++) {
+
+	for( int i = 0; i < 10; i++) {
 		data_deviation.acceleration.acc_x += pow(data_raw[i].acceleration.acc_x - mean.acceleration.acc_x, 2);
 		data_deviation.acceleration.acc_y += pow(data_raw[i].acceleration.acc_y - mean.acceleration.acc_y, 2);
 		data_deviation.acceleration.acc_z += pow(data_raw[i].acceleration.acc_z - mean.acceleration.acc_z, 2);
-		
+
 		data_deviation.color.ir 	+= pow(data_raw[i].color.ir 	- mean.color.ir, 	2);
 		data_deviation.color.clear 	+= pow(data_raw[i].color.clear 	- mean.color.clear, 2);
 		data_deviation.color.red 	+= pow(data_raw[i].color.red 	- mean.color.red, 	2);
 		data_deviation.color.green 	+= pow(data_raw[i].color.green 	- mean.color.green, 2);
 		data_deviation.color.blue 	+= pow(data_raw[i].color.blue 	- mean.color.blue, 	2);
 	}
-	data_deviation.acceleration.acc_x = sqrt(deviation.acceleration.acc_x / 10);
-	data_deviation.acceleration.acc_y = sqrt(deviation.acceleration.acc_y / 10);
-	data_deviation.acceleration.acc_z = sqrt(deviation.acceleration.acc_z / 10);
-	
-	data_deviation.color.ir = sqrt(deviation.color.ir / 10);
-	data_deviation.color.clear = sqrt(deviation.color.clear / 10);
-	data_deviation.color.red = sqrt(deviation.color.red / 10);
-	data_deviation.color.green = sqrt(deviation.color.green / 10);
-	data_deviation.color.blue = sqrt(deviation.color.blue / 10);
+	data_deviation.acceleration.acc_x = sqrt(data_deviation.acceleration.acc_x / 10);
+	data_deviation.acceleration.acc_y = sqrt(data_deviation.acceleration.acc_y / 10);
+	data_deviation.acceleration.acc_z = sqrt(data_deviation.acceleration.acc_z / 10);
+
+	data_deviation.color.ir = sqrt(data_deviation.color.ir / 10);
+	data_deviation.color.clear = sqrt(data_deviation.color.clear / 10);
+	data_deviation.color.red = sqrt(data_deviation.color.red / 10);
+	data_deviation.color.green = sqrt(data_deviation.color.green / 10);
+	data_deviation.color.blue = sqrt(data_deviation.color.blue / 10);
 }
 
 void print_stadistics(t_rcv_data mean, t_rcv_data max, t_rcv_data min, t_rcv_data deviation){
 	/*  Mean messages  */
 	printf("Mean\n\r Acceleration: \n\r");
 	printf("X: %.2f / Y: %.2f / Z: %.2f\n\r", mean.acceleration.acc_x, mean.acceleration.acc_y, mean.acceleration.acc_z);
-	
+
 	printf("Colors: \033[?25l \n\r");
 	printf("\033[38;2;255;0;0mR: %.0f  \n\r", mean.color.red);
 	printf("\033[38;2;0;255;0mG: %.0f   \n\r", mean.color.green);
 	printf("\n\033[38;2;0;0;255mB: %.0f  \n\r", mean.color.blue);
 	printf("\033[2A\033[38;2;255;255;255m");
-	
+
 	printf("\n---------------------------------------------------------------------");
 	/*  Maximum messages  */
 	printf("Maximum\n\r Acceleration: \n\r");
 	printf("X: %.2f / Y: %.2f / Z: %.2f\n\r", max.acceleration.acc_x, max.acceleration.acc_y, max.acceleration.acc_z);
-	
+
 	printf("Colors: \033[?25l \n\r");
 	printf("\033[38;2;255;0;0mR: %.0f  \n\r", max.color.red);
 	printf("\033[38;2;0;255;0mG: %.0f   \n\r", max.color.green);
 	printf("\n\033[38;2;0;0;255mB: %.0f  \n\r", max.color.blue);
 	printf("\033[2A\033[38;2;255;255;255m");
-	
+
 	printf("\n---------------------------------------------------------------------");
 	/*  Minimum messages  */
 	printf("Minimum\n\r Acceleration: \n\r");
 	printf("X: %.2f / Y: %.2f / Z: %.2f\n\r", min.acceleration.acc_x, min.acceleration.acc_y, min.acceleration.acc_z);
-	
+
 	printf("Colors: \033[?25l \n\r");
 	printf("\033[38;2;255;0;0mR: %.0f  \n\r", min.color.red);
 	printf("\033[38;2;0;255;0mG: %.0f   \n\r", min.color.green);
 	printf("\n\033[38;2;0;0;255mB: %.0f  \n\r", min.color.blue);
 	printf("\033[2A\033[38;2;255;255;255m");
-	
+
 	printf("\n---------------------------------------------------------------------");
 	/*  Stantard Deviation messages  */
 	printf("Standard Deviation: \n\r Acceleration: \n\r");
 	printf("X: %.2f / Y: %.2f / Z: %.2f\n\r", deviation.acceleration.acc_x, deviation.acceleration.acc_y, deviation.acceleration.acc_z);
-	
+
 	printf("Colors: \033[?25l \n\r");
 	printf("\033[38;2;255;0;0mR: %.0f  \n\r", deviation.color.red);
 	printf("\033[38;2;0;255;0mG: %.0f   \n\r", deviation.color.green);
 	printf("\n\033[38;2;0;0;255mB: %.0f  \n\r", deviation.color.blue);
 	printf("\033[2A\033[38;2;255;255;255m");
-	
+
 	printf("\n---------------------------------------------------------------------");
 }
